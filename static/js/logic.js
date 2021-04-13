88b057355d129e47d9ee68a7eb133fc81c306c17
Fedeli-Luke <72054916+Fedeli-Luke@users.noreply.github.com>
2021-04-12 21:58:23 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
-// Store the API endpoint inside queryUrl and queryPlatesUrl 
+// API endpoints 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-var queryPlatesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var tectonic_queryUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-// Perform a GET request to the query URL, pull data for quakes
+// Request to get URL for data
 d3.json(queryUrl, function(data) {
     console.log(data.features);
 
@@ -10,58 +10,58 @@ d3.json(queryUrl, function(data) {
     tectonicPlates(data.features);
 });
 
-// Perform a GET request to the query URL, pull data for plates
-function tectonicPlates(platesData) {
-    d3.json(queryPlatesUrl, function(data) {
+// Request to get URL for tectonic data
+function tectonicPlates(tectonicData) {
+    d3.json(tectonic_queryUrl, function(data) {
 
 
-        createFeatures(platesData, data.features);
+        createFeatures(tectonicData, data.features);
     });
 }
 
-// Reflect the magnitude of the earthquake in color 
-function magnitudeColor(magnitude) {
+// Color of quake magnitude 
+function magColor(magnitude) {
     switch (true) {
         case magnitude >= 5:
-            return '#d35400';
+            return '#d30000';
         case magnitude >= 4:
-            return '#e67e22';
+            return '#d36200';
         case magnitude >= 3:
-            return '#f39c12';
+            return '#d3a500';
         case magnitude >= 2:
-            return '#f5b041';
+            return '#d3cf00';
         case magnitude >= 1:
-            return '#f7dc6f';
+            return '#7bd300';
         default:
-            return '#7dcea0';
+            return '#1cd300';
     };
 };
 
 
-// Reflect the magnitude of the earthquake in size 
-function magnitudeSize(magnitude) {
-    return magnitude * 30000;
+// Size of quake 
+function magSize(magnitude) {
+    return magnitude * 25000;
 };
 
 // Create Features
-function createFeatures(earthquakeData, rockData) {
+function createFeatures(quakeData, dpthData) {
 
-    // For each feature, create a popup to display place, time and magnitude of each earthquake
+    // Popup to display quake information
     function onFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
             "</h3><hr><p>" + new Date(feature.properties.time) +
             "</p><hr><p>" + "<h3>Magnitude: " + feature.properties.mag + "</h3>")
 
     };
-    //Create a GeoJSON layer containing the features array on the earthquakeData object
-    var earthquakes = L.geoJSON(earthquakeData, {
+    //GeoJSON layer containing the features 
+    var earthquakes = L.geoJSON(quakeData, {
 
         onEachFeature: onFeature,
 
         pointToLayer: function(feature, latlng) {
             return L.circle(latlng, {
-                radius: magnitudeSize(feature.properties.mag),
-                fillColor: magnitudeColor(feature.properties.mag),
+                radius: magSize(feature.properties.mag),
+                fillColor: magColor(feature.properties.mag),
                 fillOpacity: 0.8,
                 color: "#000000",
                 weight: 0.5
@@ -70,9 +70,8 @@ function createFeatures(earthquakeData, rockData) {
 
     });
 
-
-    // Create a GeoJSON layer containing the rockData
-    var faultLines = L.geoJson(rockData, {
+    //GeoJSON layer 
+    var faultLines = L.geoJson(dpthData, {
         style: function(feature) {
             var latlngs = (feature.geometry.coordinates);
             return L.polyline(latlngs);
@@ -84,7 +83,7 @@ function createFeatures(earthquakeData, rockData) {
 
 function createMap(earthquakes, faultLines) {
 
-    // Define satellite, grayscale and outdoors layers
+    // Map layers
     var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
@@ -106,48 +105,43 @@ function createMap(earthquakes, faultLines) {
         accessToken: API_KEY
     });
 
-    // Define a baseMaps object to hold our base layers
+    // Object to hold map layers
     var baseMaps = {
         "Satellite": satellite,
         "Grayscale": grayscale,
         "Outdoors": outdoors
     };
 
-    // Define a baseMaps object to hold our base layers
     var overlayMaps = {
         Earthquakes: earthquakes,
         FaultLines: faultLines
     };
 
-    // Create a new map
+    // Create a map
     var myMap = L.map("map", {
         center: [15.00, -20.00],
         zoom: 2.5,
         layers: [satellite, grayscale, outdoors, earthquakes, faultLines]
     });
 
-    // Create a layer control containing our baseMaps
-    //And the overlay Layer containing the earthquake GeoJSON 
+    // Adding layers to map
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 
-    // Set up the legend
+    // Creating the legend
     var legend = L.control({ position: 'bottomright' });
 
     legend.onAdd = function() {
 
         var div = L.DomUtil.create('div', 'info legend'),
+            mag_scale = [0, 1, 2, 3, 4, 5];
 
-            // Define the the scale of the earthquake for the legend
-            magnitude_scale = [0, 1, 2, 3, 4, 5];
-
-
-        // Add a row to the legend for each "magnitude_scale"
-        for (var i = 0; i < magnitude_scale.length; i++) {
+        // Add a row to the legend
+        for (var i = 0; i < mag_scale.length; i++) {
 
             div.innerHTML +=
-                '<i style="background:' + magnitudeColor(magnitude_scale[i]) + '"></i> ' +
-                magnitude_scale[i] + (magnitude_scale[i + 1] ? '&ndash;' + magnitude_scale[i + 1] + '<br>' : '+');
+                '<i style="background:' + magColor(mag_scale[i]) + '"></i> ' +
+                mag_scale[i] + (mag_scale[i + 1] ? '&ndash;' + mag_scale[i + 1] + '<br>' : '+');
 
         }
         return div;
